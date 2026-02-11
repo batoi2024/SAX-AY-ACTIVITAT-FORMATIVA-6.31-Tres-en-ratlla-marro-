6b705a9b24c421a1393bd1941e8a08d3d020f140
batoi2024 <miguelitopuntocom@gmail.com>
2026-02-11 17:33:22 +0100

    Estructura inicial del joc Tres en Ratlla (Part 1)

---
+++ TresEnRatlla.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Tres_Raya;
+
+/**
+ *
+ * @author batoi
+ */
+public class TresEnRatlla {
+    
+    
+    public static void main(String[] args) {
+        
+        //1. creamos la estancia de la logica del juego 
+        TresEnRatlla partida = new TresEnRatlla();
+          
+        //2. Iniciamos el flujo de la partida
+        partida.jugar();
+        
+        
+        
+    }
+
+    private void jugar() {
+      
+    }
+    
+}
